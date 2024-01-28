b48be9a3b82fd085964dc699684fb98a7f47cfa2
AneeqaWaheed <aneeqawaheed19@gmail.com>
2024-01-28 19:38:14 +0500

    About Section

--- aneeqawaheed/src/App.js
+++ aneeqawaheed/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
+import About from './Components/About';
 
 function App() {
   return (
    <>
    <Navbar />
    <Home text="Aneeqa Waheed's Portfolio " delay={100} />
+   <About />
    </>
   );
 }
