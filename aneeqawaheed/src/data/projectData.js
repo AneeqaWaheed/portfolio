7c7d49a0c7824f328533e7d6183ad790e91f17bc
AneeqaWaheed <aneeqawaheed19@gmail.com>
2024-01-29 10:13:03 +0500

    project modal

--- aneeqawaheed/src/data/projectData.js
+++ aneeqawaheed/src/data/projectData.js
@@ -2,39 +2,11 @@ const projects = [
     {
         id: 1,
         name: 'Project 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta?',
-        imageUrl: './assets/project1.jpg',
+        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        imageUrl: './assets/1.jpg',
+        images: ['./assets/coding.jpg', './assets/coding.png', './assets/project1_image3.jpg'],
         viewLink: 'https://example.com/project1',
       },
-      {
-        id: 1,
-        name: 'Project 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta?',
-        imageUrl: './assets/project1.jpg',
-        viewLink: 'https://example.com/project1',
-      },
-      {
-        id: 1,
-        name: 'Project 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta?',
-        imageUrl: './assets/project1.jpg',
-        viewLink: 'https://example.com/project1',
-      },
-      {
-        id: 1,
-        name: 'Project 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta?',
-        imageUrl: './assets/project1.jpg',
-        viewLink: 'https://example.com/project1',
-      },
-      {
-        id: 1,
-        name: 'Project 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta?',
-        imageUrl: './assets/project1.jpg',
-        viewLink: 'https://example.com/project1',
-      },
-      // Add more projects as needed
   ];
   
 export default projects;
