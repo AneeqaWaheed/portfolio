b60aeff52a9eb8a25cb0c1fa7b2a4d70ba28d998
AneeqaWaheed <aneeqawaheed19@gmail.com>
2024-04-07 18:04:47 +0500

    Technologies

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -4,11 +4,13 @@ import Home from '../Components/Home';
 import About from '../Components/About';
 import Projects from '../Components/Projects';
 import Footer from '../Components/Footer';
+import Technologies from '../Components/Technologies';
 function Main() {
   return (
     <div className="landing-page ">
      <Navbar />
    <Home text="Aneeqa Portfolio " delay={100} />
+   <Technologies/>
    <About />
    <Projects />
    <Footer />
