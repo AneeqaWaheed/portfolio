0fa61669b8524306bc408537913755259b16dda8
AneeqaWaheed <aneeqawaheed19@gmail.com>
2024-04-07 20:53:14 +0500

    Email.js contact form

--- src/Components/About.js
+++ src/Components/About.js
@@ -6,12 +6,12 @@ import './CSS/about.css';
 const YourComponent = () => {
   return (
     <div id="about-section" className="container d-flex justify-content-center align-items-center flex-column h-100 my-4" >
-      <h1 className="text-center ">About</h1>
+      <h1 className="text-center " style={{ color: 'orange' }}>About</h1>
       <div className="row mt-4">
         {/* Skills Section */}
         <div className="section skills-section">
           <h2 className="fw-medium text-center text-white">My Skills</h2>
-          <p className='text-white-50'>
+          <p className='text-white'>
           I'm a dedicated web and app developer with a passion for coding.
             As a tech enthusiast, I specialize in a variety of technologies, including:
             ReactJS
@@ -28,7 +28,7 @@ const YourComponent = () => {
         {/* Journey Section */}
         <div className="section journey-section">
           <h2 className="fw-medium text-center text-white">My Journey</h2>
-          <p className='text-white-50'> My journey in the world of development is fueled by a genuine love for creating digital solutions that make a difference. Whether it's crafting responsive web interfaces or building robust mobile applications, I bring a creative and solution-driven approach to every project.</p>
+          <p className='text-white'> My journey in the world of development is fueled by a genuine love for creating digital solutions that make a difference. Whether it's crafting responsive web interfaces or building robust mobile applications, I bring a creative and solution-driven approach to every project.</p>
         </div>
       </div>
     </div>
