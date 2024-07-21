7a1ee610c499c6cfb849bc7a769648dd346cc6c2
Aneeqa Waheed <122236019+AneeqaWaheed@users.noreply.github.com>
2024-07-21 10:48:23 +0500

    remove unneccessary imports

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/Main';
-import ContactForm from './Components/Contact';
+
 
 function App() {
   return (
